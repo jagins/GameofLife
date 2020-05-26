dec6f8aed4717a9099de38aed030ad034de6200b
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-26 13:07:32 -0500

    basic framework setup working on getting the canvas setup next

--- lifegame/src/App.js
+++ lifegame/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Canvas from './components/Canvas';
+import Button from './components/Button';
+import Rules from './components/Rules'
 import './App.css';
 
-function App() {
+function App() 
+{
   return (
     <div className="App">
-      
+      <h1>Conway's Game of Life</h1>
+      <h3>Generation: #</h3>
+      <Canvas/>
+      <Button text='Play'/>
+      <Button text='Pause'/>
+      <Button text='Stop'/>
+      <Rules/>
     </div>
   );
 }
---
+++ lifegame/src/components/Canvas.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Canvas()
+{
+    return (
+        <canvas></canvas>
+    );
+}
+
+export default Canvas;
