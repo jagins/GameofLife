c95ca6b1c4638c052ba55ed60ca97bd1e98fdd56
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-26 15:52:06 -0500

    have a grid drawing to the screen

--- lifegame/src/components/Canvas.js
+++ lifegame/src/components/Canvas.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, {useRef,useEffect} from 'react';
 
 function Canvas()
 {
+    const canvasRef = useRef(null);
+
+    const drawGrid = (ctx) =>
+    {
+        ctx.beginPath();
+        ctx.fillStyle = 'white';
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = 'black';
+
+        for(let row = 0; row < 25; row++)
+        {
+            for(let column = 0; column < 25; column++)
+            {
+                const x = column * 20;
+                const y = row * 20;
+
+                ctx.rect(x,y,20,20);
+                ctx.fill();
+                ctx.stroke();
+            }
+        }
+
+        ctx.closePath();
+    }
+
+    useEffect(() =>
+    {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+        drawGrid(ctx);
+    })
     return (
-        <canvas></canvas>
+       <canvas
+        ref={canvasRef}
+        width='600px'
+        height='600px'
+       />
     );
 }
 
