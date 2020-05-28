91224e5564c7dc00f08a136a587406da95684a07
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-05-28 12:28:09 -0500

    added buttons to p5 and generation counter

--- lifegame/src/components/Canvas.js
+++ lifegame/src/components/Canvas.js
@@ -1,13 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
-import sketch from '../sketches/sketch';
+import {sketch} from '../sketches/sketch';
 
 function Canvas()
 {
-    const [generation, setGeneration] = useState(0);
     return (
         <div className='canvasDiv'>
-            <h3>Generation: {generation}</h3>
             <P5Wrapper sketch={sketch}/>
         </div>
     );
--- lifegame/src/sketches/sketch.js
+++ lifegame/src/sketches/sketch.js
@@ -1,10 +1,31 @@
-export default function sketch(p)
-{   
-    const create2dArray = (cols, rows) =>
-    {
+export function sketch(p) {
+    let isStart = false;
+    const startSketch = () => {
+        isStart = true;
+    }
+
+    const stopSketch = () => {
+        isStart = false;
+    }
+
+    const reset = () => {
+        isStart = false;
+        generation = 0;
+        const selp = p.select('p');
+        selp.html(`Generation: ${generation}`);
+        cols = p.width / resolution;
+        rows = p.height / resolution;
+
+        grid = create2dArray(cols, rows);
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
+                grid[i][j] = p.floor(p.random(2));
+            }
+        }
+    }
+    const create2dArray = (cols, rows) => {
         let arr = new Array(cols);
-        for(let i = 0; i < arr.length; i++)
-        {
+        for (let i = 0; i < arr.length; i++) {
             arr[i] = new Array(rows);
         }
         return arr;
@@ -14,36 +35,57 @@ export default function sketch(p)
     let cols;
     let rows;
     let resolution = 20;
+    let generation = 0;
 
-    p.setup = () =>
-    {
+    p.setup = () => {
         p.createCanvas(600, 600);
-        cols = p.width / resolution;
-        rows = p.height / resolution;
+        const startButton = p.createButton('Start');
+        const stopButton = p.createButton('Stop');
+        const resetButton = p.createButton('Reset');
+        const paragraph = p.createP(`Generation: ${generation}`)
+        startButton.mousePressed(startSketch);
+        stopButton.mousePressed(stopSketch);
+        resetButton.mousePressed(reset);
+        reset();
+        runSimulation();
+    }
 
-        grid = create2dArray(cols, rows);
-        for(let i = 0; i < cols; i++)
+    p.draw = () => {
+        p.background(0);
+        const selp = p.select('p');
+        if (isStart) {
+            runSimulation();
+            selp.html(`Generation: ${generation}`);
+        }
+    }
+
+    const countNeighbors = (grid, x, y) => 
+    {
+        let sum = 0;
+        for (let i = -1; i < 2; i++) 
         {
-            for(let j = 0; j < rows; j++)
+            for (let j = -1; j < 2; j++) 
             {
-                grid[i][j] = p.floor(p.random(2));
+                let col = (x + i + cols) % cols;
+                let row = (y + j + rows) % rows;
+                sum += grid[col][row];
             }
         }
-    }   
 
-    p.draw = () =>
+        sum -= grid[x][y];
+
+        return sum;
+    }
+
+    const runSimulation = () =>
     {
-        p.background(0);
-        for(let i = 0; i < cols; i++)
-        {
-            for(let j = 0; j < rows; j++)
-            {
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
                 let x = i * resolution;
                 let y = j * resolution;
-                
-                if(grid[i][j] === 1)
-                {
-                   p.fill(255);
+
+                if (grid[i][j] === 1) {
+                    p.fill(255);
                     p.stroke(0);
                     p.rect(x, y, resolution - 1, resolution - 1);
                 }
@@ -51,47 +93,27 @@ export default function sketch(p)
         }
 
         let next = create2dArray(cols, rows);
-        for(let i = 0; i < cols; i++)
-        {
-            for(let j = 0; j < rows; j++)
-            {
+        for (let i = 0; i < cols; i++) {
+            for (let j = 0; j < rows; j++) {
                 let state = grid[i][j];
-                
+
                 let sum = 0;
-                    let neighbors = countNeighbors(grid, i, j);
-                if(state === 0 && neighbors === 3)
-                {
+                let neighbors = countNeighbors(grid, i, j);
+                if (state === 0 && neighbors === 3) {
                     next[i][j] = 1;
                 }
-                else if(state === 1 && (neighbors < 2 || neighbors > 3))
-                {
+                else if (state === 1 && (neighbors < 2 || neighbors > 3)) {
                     next[i][j] = 0
                 }
-                else
-                {
+                else {
                     next[i][j] = state;
                 }
             }
         }
-    
+
         grid = next;
+        generation++;
     }
 
-    const countNeighbors = (grid, x, y) =>
-    {
-        let sum = 0;
-        for(let i = -1; i < 2; i++)
-        {
-            for(let j = -1; j < 2; j++)
-            {
-            let col = (x + i + cols) % cols;
-            let row = (y + j + rows) % rows;
-            sum += grid[col][row];
-            }
-        }
-  
-        sum -= grid[x][y];
-  
-        return sum;
-    }
+
 }
